daee7117216ed38df0f3685762024e8195d43884
Marcelo Carvalho <carvalhoms@icloud.com>
2022-02-11 14:48:50 -0300

    Module e Service Prisma Obs.: Está com erro!!!

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+import { PrismaService } from './prisma.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [PrismaService],
+})
+export class PrismaModule {}
